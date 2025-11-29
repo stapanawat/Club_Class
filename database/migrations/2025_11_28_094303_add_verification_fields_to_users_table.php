4b63bb0f7de3dd04e334ae326670fb8a8963f258
bright <stapanawatjaito@gmail.com>
2025-11-29 23:26:50 +0700

    Add missing personal_access_tokens migration

--- database/migrations/2025_11_28_094303_add_verification_fields_to_users_table.php
+++ database/migrations/2025_11_28_094303_add_verification_fields_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('selected_plan')->default('free');
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('selected_plan');
         });
     }
 };
